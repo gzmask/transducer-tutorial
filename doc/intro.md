94028ff1cf090185253b423ae0c63b71d55f9158
ray-lei <r.lei@sap.com>
2020-03-03 13:52:27 -0800

    template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to transducer_tut
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/transducer_tut/core_test.clj
@@ -0,0 +1,7 @@
+(ns transducer-tut.core-test
+  (:require [clojure.test :refer :all]
+            [transducer-tut.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
